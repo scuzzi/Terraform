6b97cfe102f1ed75d6012fffb800ae63f6196f22
Mike <mcsmithjr25@gmail.com>
2025-12-21 21:37:44 -0500

    Fix web server configuration with index.html

--- main.tf
+++ main.tf
@@ -5,14 +5,58 @@ region = "us-east-2"
 
 }
 
+
+ variable "server_port" {
+        description = "The port the server will use for HTTP requests"
+        type = number
+        default = 8080
+}
+
+
 resource "aws_instance" "example" {
+        ami                    = "ami-0fb653ca2d3203ac1"
+        instance_type          = "t2.micro"
+        vpc_security_group_ids = [aws_security_group.instance.id]
 
-    ami = "ami-0fb653ca2d3203ac1"
-    instance_type = "t2.micro"
+        user_data = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World" > index.html
+                    nohup busybox httpd -f -p ${var.server_port} &
+                    EOF
 
-    tags = {
-   
-        Name = "terraform-example"   
-    }
+        user_data_replace_on_change = true
 
+        tags = {
+            Name = "terraform-example"
+        }
+}
+
+resource "aws_security_group" "instance" {
+        name = "terraform-example-instance"
+
+        ingress {
+            from_port   = var.server_port
+            to_port     = var.server_port
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        ingress {
+            from_port   = 22
+            to_port     = 22
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress {
+            from_port   = 0
+            to_port     = 0
+            protocol    = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+}
+
+output "public_ip" {
+value = aws_instance.example.public_ip
+description = "The public IP address of the web server"
 }
