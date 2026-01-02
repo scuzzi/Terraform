6381482e02d6d7537e20baa1335131c2c1293a20
Mike <mcsmithjr25@gmail.com>
2026-01-02 14:31:58 -0500

    Commented out single example instance

--- main.tf
+++ main.tf
@@ -47,24 +47,26 @@ resource "aws_instance" "example" {
         }
 }
 
-resource "aws_launch_template" "example" {
-  name_prefix   = "terraform-example-"
-  image_id      = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
+#name_prefix   = "terraform-example-"
+#  image_id      = "ami-0fb653ca2d3203ac1"
+# instance_type = "t2.micro"
+# 
+ #  vpc_security_group_ids = [aws_security_group.instance.id]
+# 
+ #  user_data = base64encode(<<-EOF
+ #              #!/bin/bash
+ #              echo "Hello, World" > index.html
+ #              EOF
+ #  )
+ # 
+ # lifecycle {
+  #  create_before_destroy = true
+ # }
+#}
 
-  vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = base64encode(<<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-  )
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+
 
 resource "aws_autoscaling_group" "example"{
     vpc_zone_identifier = data.aws_subnets.default.ids
